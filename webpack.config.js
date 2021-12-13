3f3dc9859f6e9fbfe78ce9b3bcb56ce8fd85c3f6
bilgeonceken <oncekenbilge@gmail.com>
2021-12-13 22:25:49 +0300

    Core :: use file-loader instead of svg-inline-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,13 +6,13 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: 'file-loader',
+      },
+      {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader',
       },
     ],
   },
