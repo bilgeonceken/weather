4c7e76ce5dd9e5a4e5287a0a3fadafbae3221862
bilgeonceken <oncekenbilge@gmail.com>
2021-12-15 06:48:26 +0300

    Store :: format and pass datetime to weather data

--- src/store/saga/index.js
+++ src/store/saga/index.js
@@ -13,14 +13,23 @@ export function* watchFetchForecast() {
   const { city: { name: location }, list } = yield call(fetchForecast);
   const convertKtoC = (temperature) => Math.floor(temperature - 273.15);
   const weatherData = list.map(({
-    main: { temp, temp_min: tempMin, temp_max: tempMax }, weather: { main: weather },
+    main: { temp, temp_min: tempMin, temp_max: tempMax }, weather: [{ main: weather }],
+    dt_txt: datetime,
   }) => {
+    const time = datetime.split(' ')[1];
+    const date = new Date(datetime);
+    // possibly not the best way to get de-DE date format in english
+    const readableDateString = date.toLocaleDateString('en-US', { weekday: 'long', month: 'long', day: 'numeric' });
+    const [weekday, month, day] = readableDateString.replace(',', '').split(' ');
+    const formattedDate = `${weekday} ${day}. ${month}`;
     return {
       temp: convertKtoC(temp),
       tempMin: convertKtoC(tempMin),
       tempMax: convertKtoC(tempMax),
       weather,
       location,
+      time: time.match(/.{2}:.{2}/)[0],
+      date: formattedDate,
     };
   });
   yield put(setForecastAction({ location, weatherData }));
