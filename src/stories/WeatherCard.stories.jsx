2b019c013d3383c0268aa5786299768795547374
bilgeonceken <oncekenbilge@gmail.com>
2021-12-15 09:14:29 +0300

    Story :: WeatherCard :: update component path

--- src/stories/WeatherCard.stories.jsx
+++ src/stories/WeatherCard.stories.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import WeatherCard from '../components/WeatherCard';
+import WeatherCard from '../components/Forecast/WeatherCard';
 import { WEATHER_TYPES } from '../utils/constants';
 
 export const Clear = () => {
