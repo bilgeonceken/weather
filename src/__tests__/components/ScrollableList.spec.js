ff83ba8f343ddcd09fd2648f11bf25a9094b177b
bilgeonceken <oncekenbilge@gmail.com>
2021-12-17 21:36:46 +0300

    Tests :: ScrollableList

---
+++ src/__tests__/components/ScrollableList.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import ScrollableList from '../../components/ScrollableList';
+
+const dummyData = ['first', 'second'];
+const renderDummyChildren = () => dummyData.map((data) => (<div>{data}</div>));
+
+describe('ScrollableList', () => {
+  it('should render children', () => {
+    const { container } = render(<ScrollableList>{renderDummyChildren()}</ScrollableList>);
+    const list = container.getElementsByTagName('ul')[0];
+    expect(list.childElementCount).toBe(dummyData.length);
+  });
+});
